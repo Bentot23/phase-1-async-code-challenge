c98364a150df0b75269280d540e56cbbad7ed8b8
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-08-17 15:07:52 -0500

    first commit, be able to list the shows

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code here
+const baseUrl = 'http://localhost:3000/shows'
+const showLists = document.querySelector('#list')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM Loaded!')
+    getShows()
+})
+
+const getShows = () => {
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        showLists.innerHTML = ''
+        data.map(show => {
+            console.log(show)
+            showLists.innerHTML += `<li>${show.title}</li>`
+        })
+    })
+    
+}
