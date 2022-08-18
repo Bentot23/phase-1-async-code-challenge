36184a1c077bc982eb615d473c688c9b2e22b67a
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2022-08-18 01:12:01 -0500

    final commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const queuedShowListsContainer = document.querySelector('#queued')
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Loaded!')
     getShows()
-
+    
 })
 
 const getShows = () => {
@@ -20,41 +20,46 @@ const getShows = () => {
             // console.log(show)
             showListsContainer.innerHTML += 
             `<li id="showList" href="#" data-title="${show.title}">${show.title}</li>`
-            
         })
-        clickToLink()
+        addToQueued()
+        searchShow()
 
     })
 }
 
-const clickToLink = () => {
+const addToQueued = () => {
     const showLists = document.querySelectorAll('li#showList')
       showLists.forEach(lists => {
-        console.log(lists)
+        // console.log(lists)
         lists.addEventListener('click', (e) => {
             // console.log('Clicked')
             // console.log(e.target.dataset.title)
             queuedShowListsContainer.innerHTML += `<li id="queuedList" href="#" data-title="${e.target.dataset.title}">${e.target.dataset.title}</li>`
         removeQueued()
-
         })
-
     })
-
 }
-
 const removeQueued = () => {
     const queuedLists = document.querySelectorAll('li#queuedList')
     queuedLists.forEach(lists => {
-        console.log(lists)
+        // console.log(lists)
         lists.addEventListener('click', (e) => {
             // console.log(e.target)
-            e.target.remove()
-            
+            e.target.remove()  
         })
     })
 }
 
 const searchShow = () => {
+    const btn = document.querySelector('button#search-button')
+    const inputValue = document.querySelector('#search')
+    let showLists = document.querySelectorAll('li#showList')
+
+    btn.addEventListener('click', () => {
+        showListsContainer.innerHTML = ''
+        showListsContainer.innerHTML = `<li>${inputValue.value}</li>`
+
+    }) 
+    addToQueued()
 
 }
